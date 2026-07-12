9a934456f3ba12df1027707ee610d1f63e71df33
AdelAlmuhtaseb <AtelAlmuhtaseb@gmail.com>
2026-07-12 21:53:29 +0300

    Fix unresolved merge conflict markers in client.js

--- dashboard/src/api/client.js
+++ dashboard/src/api/client.js
@@ -1,14 +1,5 @@
 import axios from "axios";
-
 const client = axios.create({
-<<<<<<< HEAD
   baseURL: "https://heritage-trace-2.onrender.com/api",
 });
-
 export default client;
-=======
-  baseURL: "http://127.0.0.1:5000/api",
-});
-
-export default client;
->>>>>>> origin/main
